077dd5aeb4dc32868763bd97d14a631acdca1d14
Necip Allef <necipallef@gmail.com>
2024-07-11 19:44:17 +0300

    fix: required providers

--- misc.tf
+++ misc.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    random_id = {
+      source = "hashicorp/random"
+      version = "~> 3.6.2"
+    }
+  }
+}
+
 resource "random_id" "integration_id" {
   byte_length = 6
 }
